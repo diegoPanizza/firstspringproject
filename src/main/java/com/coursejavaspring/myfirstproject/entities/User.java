8d30079861b6c708982ce2842bb54750b2e2a809
Diego Panizza <diego_panizza@hotmail.com>
2023-07-04 11:24:22 -0300

    Project created

---
+++ src/main/java/com/coursejavaspring/myfirstproject/entities/User.java
@@ -0,0 +1,10 @@
+package com.coursejavaspring.myfirstproject.entities;
+
+public class User {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+}
