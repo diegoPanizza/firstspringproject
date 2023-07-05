722b113a3013d4bd7429d68d1f52278ae0334499
Diego Panizza <diego_panizza@hotmail.com>
2023-07-05 10:22:57 -0300

    H2 database, test profile, JPA

--- src/main/java/com/coursejavaspring/myfirstproject/entities/User.java
+++ src/main/java/com/coursejavaspring/myfirstproject/entities/User.java
@@ -2,6 +2,11 @@ package com.coursejavaspring.myfirstproject.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,9 +19,14 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+
+@Entity
+@Table(name = "tb_user")
 public class User  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
